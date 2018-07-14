f6ee575b39fa4f52d68f5c43e81043c031a1ac10
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2018-07-14 11:20:30 +0800

    UserDefaults

---
+++ Table Clicker/ClickerViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Table Clicker
+//
+//  Created by Soon Yin Jie on 14/7/18.
+//  Copyright © 2018 Tinkercademy. All rights reserved.
+//
+
+import UIKit
+
+class ClickerViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    var counter = 0
+    var time: Float = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (_) in
+            self.time += 0.1
+        }
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func tapped(_ sender: Any) {
+        counter += 1
+        label.text = "\(counter)"
+        
+        if counter == 30 {
+            print("Time is \(time)")
+            performSegue(withIdentifier: "exitClicker", sender: self)
+        }
+    }
+    
+}
+
